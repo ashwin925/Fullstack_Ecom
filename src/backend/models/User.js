3f7bf0f126fbf3939c98926a82edcc7945d21e32
ashwin925 <ashwinsundar90250@gmail.com>
2025-03-01 07:07:33 +0530

    backend

--- src/backend/models/User.js
+++ src/backend/models/User.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    password: { type: String }, // Not required for Google users
     role: { type: String, enum: ['customer', 'seller', 'admin'], default: 'customer' },
-    mfaSecret: { type: String }
+    googleId: { type: String }, // Google ID added
 });
 
 module.exports = mongoose.model('User', UserSchema);
