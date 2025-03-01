b0a154ebbbe1fe4d0524e3af77e7bea2d5f57aca
ashwin925 <ashwinsundar90250@gmail.com>
2025-03-01 07:06:59 +0530

    backend update

--- src/backend/config/passport.js
+++ src/backend/config/passport.js
@@ -2,29 +2,33 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('../models/User');
 
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/api/auth/google/callback"
-}, async (accessToken, refreshToken, profile, done) => {
-    try {
-        let user = await User.findOne({ googleId: profile.id });
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            callbackURL: `${process.env.SERVER_URL}/api/auth/google/callback`
+        },
+        async (accessToken, refreshToken, profile, done) => {
+            try {
+                let user = await User.findOne({ email: profile.emails[0].value });
 
-        if (!user) {
-            user = new User({
-                googleId: profile.id,
-                name: profile.displayName,
-                email: profile.emails[0].value,
-                password: null,  // No password needed for Google Auth
-            });
-            await user.save();
+                if (!user) {
+                    user = new User({
+                        googleId: profile.id,
+                        name: profile.displayName,
+                        email: profile.emails[0].value,
+                    });
+                    await user.save();
+                }
+
+                return done(null, user);
+            } catch (err) {
+                return done(err, null);
+            }
         }
-
-        return done(null, user);
-    } catch (err) {
-        return done(err, null);
-    }
-}));
+    )
+);
 
 passport.serializeUser((user, done) => {
     done(null, user.id);
