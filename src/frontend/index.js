61dfcb09e07721658c5436ef1289829be268590f
ashwin925 <ashwinsundar90250@gmail.com>
2025-03-02 11:01:03 +0530

    setup

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 
 export default function Home() {
     const navigate = useNavigate();
