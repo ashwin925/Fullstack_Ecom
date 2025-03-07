c793bf962d24ae1777b5bc324fbf8844cf0a9e5b
ashwin925 <ashwinsundar90250@gmail.com>
2025-03-07 11:40:32 +0530

    updates

--- src/backend/server.js
+++ src/backend/server.js
@@ -3,11 +3,18 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/auth.js";
+const cors = require("cors");
 
 dotenv.config();
 connectDB();
 
 const app = express();
+
+app.use(cors({
+  origin: "http://localhost:3000", // Allow frontend URL
+  credentials: true // Allow cookies if needed
+}));
+
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
