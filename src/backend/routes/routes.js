0fc1a089cbf2fbb0c9b0770349b256f451f671f8
ashwin925 <ashwinsundar90250@gmail.com>
2025-03-07 20:39:47 +0530

    common js to ES modules

--- src/backend/routes/routes.js
+++ src/backend/routes/routes.js
@@ -1,14 +1,14 @@
-const express = require("express");
-const router = express.Router();
-const { authenticate, authorize } = require("../middleware/authenticate");
+import express from "express";
+import { authenticate, authorize } from "../middleware/authenticate.js";
 
+const router = express.Router();
 
 router.get("/admin", authenticate, authorize(["admin"]), (req, res) => {
     res.json({ message: "Welcome Admin" });
-  });
-  
-  router.get("/user", authenticate, authorize(["user", "admin"]), (req, res) => {
+});
+
+router.get("/user", authenticate, authorize(["user", "admin"]), (req, res) => {
     res.json({ message: "Welcome User" });
-  });
-  
-  module.exports = router;
+});
+
+export default router; // ES module export
