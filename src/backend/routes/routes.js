f3f19363fc292c2fab28a8185e85cc9c45d0654f
ashwin925 <ashwinsundar90250@gmail.com>
2025-03-07 20:33:28 +0530

    upt

--- src/backend/routes/routes.js
+++ src/backend/routes/routes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const { verifyRole } = require("../controllers/authController");
 const router = express.Router();
+const { authenticate, authorize } = require("../middleware/authenticate");
+
 
 router.get("/admin", verifyRole(["admin"]), (req, res) => {
     res.json({ message: "Welcome Admin" });
