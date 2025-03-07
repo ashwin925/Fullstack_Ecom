c2c27d0b1c2bd49ca5c2e2f6aa6f537b0e87b6a5
ashwin925 <ashwinsundar90250@gmail.com>
2025-03-07 20:23:16 +0530

    upt

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
-import api from "./api";
-import Login from "./login";
-import Dashboard from "./dashboard";
+import api from "./frontend/api";
+import Login from "./frontend/login";
+import Dashboard from "./frontend/dashboard";
 
 export default function App() {
   const [user, setUser] = useState(null);
