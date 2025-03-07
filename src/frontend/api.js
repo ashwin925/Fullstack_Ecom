bfdf539f52b84f1c479b4c8aa8e675880f449033
ashwin925 <ashwinsundar90250@gmail.com>
2025-03-07 19:22:50 +0530

    upt

--- src/frontend/api.js
+++ src/frontend/api.js
@@ -2,7 +2,13 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: "http://localhost:5000/api",
-    withCredentials: true,
+});
+
+// Add JWT to requests
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+    return config;
 });
 
 export default api;
