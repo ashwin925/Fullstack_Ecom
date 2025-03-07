877405a65f171d8e41b7e92ea17db3b10a15034a
ashwin925 <ashwinsundar90250@gmail.com>
2025-03-07 11:20:18 +0530

    auth

--- src/backend/middleware/authenticate.js
+++ src/backend/middleware/authenticate.js
@@ -1,18 +1,16 @@
-const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
 
-exports.authenticate = async (req, res, next) => {
+export const authenticate = async (req, res, next) => {
+    const token = req.cookies.token;
+    if (!token) return res.status(401).json({ message: "Unauthorized" });
+    
     try {
-        const token = req.cookies.token;
-        if (!token) return res.status(401).json({ message: "Unauthorized" });
-
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        const user = await User.findById(decoded.id);
-        if (!user) return res.status(401).json({ message: "User not found" });
-
-        req.user = user;
+        req.user = await User.findById(decoded.id).select("-password");
+        if (!req.user) return res.status(401).json({ message: "User not found" });
         next();
     } catch (error) {
-        return res.status(403).json({ message: "Invalid token" });
+        res.status(401).json({ message: "Invalid Token" });
     }
 };
