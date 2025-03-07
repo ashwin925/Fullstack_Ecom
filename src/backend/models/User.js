2fdc6fc2833fbe872a3859fba847a1fa1f2935ee
ashwin925 <ashwinsundar90250@gmail.com>
2025-03-07 11:18:52 +0530

    upt

--- src/backend/models/User.js
+++ src/backend/models/User.js
@@ -1,13 +1,16 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
-const UserSchema = new mongoose.Schema(
-    {
-        name: { type: String, required: true },
-        email: { type: String, required: true, unique: true },
-        password: { type: String, required: true },
-        role: { type: String, enum: ["user", "admin"], default: "user" },
-    },
-    { timestamps: true }
-);
+const UserSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ["user", "admin"], default: "user" }
+});
 
-module.exports = mongoose.model("User", UserSchema); 
+UserSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
