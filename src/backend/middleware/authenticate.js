f2c206e516905acc444eb23bdd45767c0d087d89
ashwin925 <ashwinsundar90250@gmail.com>
2025-03-07 19:21:54 +0530

    upt

--- src/backend/middleware/authenticate.js
+++ src/backend/middleware/authenticate.js
@@ -2,15 +2,21 @@ import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
 export const authenticate = async (req, res, next) => {
-    const token = req.cookies.token;
+    const token = req.headers.authorization?.split(" ")[1] || req.cookies.token;
     if (!token) return res.status(401).json({ message: "Unauthorized" });
     
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = await User.findById(decoded.id).select("-password");
-        if (!req.user) return res.status(401).json({ message: "User not found" });
         next();
     } catch (error) {
         res.status(401).json({ message: "Invalid Token" });
     }
+};
+
+export const authorize = (roles) => (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+        return res.status(403).json({ message: "Forbidden" });
+    }
+    next();
 };
