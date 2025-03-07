88549215dbee6a55b356dff9d389c1638acfdf40
ashwin925 <ashwinsundar90250@gmail.com>
2025-03-07 20:32:42 +0530

    upt

--- src/backend/server.js
+++ src/backend/server.js
@@ -3,7 +3,9 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/auth.js";
-import cors from "cors"; // Change require to import
+import cors from "cors"; 
+import userRoutes from "./routes/userRoutes.js";
+import routes from "./routes/routes.js";
 
 dotenv.config();
 connectDB();
@@ -14,9 +16,12 @@ app.use(cors({
   origin: "http://localhost:3000", // Frontend URL
   credentials: true,
 }));
+
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes); // User management routes
+app.use("/api", routes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
